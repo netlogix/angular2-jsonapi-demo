97f2b50f6b8e3f07e657f6132847766ad3fbed4f
Stephan Schuler <stephan.schuler@netlogix.de>
2016-06-24 00:33:32 +0200

    Fetch json data and adjust view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import {Component} from '@angular/core';
+import {DemoItem} from "./domain/demo-item";
+import {DemoItemRepository} from "./domain/demo-item.repository";
 
 @Component({
 	moduleId: module.id,
 	selector: 'app-root',
 	templateUrl: 'app.component.html',
-	styleUrls: ['app.component.css']
+	styleUrls: ['app.component.css'],
+	providers: [
+		DemoItemRepository
+	]
 })
 export class AppComponent {
-	title = 'app works!';
+	title = 'Data loaded through @netlogix/jsonapi';
+
+	demoItems:DemoItem[] = [];
+
+	constructor(demoItemRepository:DemoItemRepository) {
+		demoItemRepository.findAll().then(demoItems => {
+			this.demoItems = demoItems;
+		});
+	}
 }
